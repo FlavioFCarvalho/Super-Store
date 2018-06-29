0db300b8eaa34ecbff81160870492291ba4f5b76
Flavio Carvalho <flavio@globalnetsis.com.br>
2018-06-29 17:57:23 -0300

    Criando components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { SeachbarComponent } from './components/seachbar/seachbar.component';
+import { CartbarComponent } from './components/cartbar/cartbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MenuComponent,
+    LogoComponent,
+    SeachbarComponent,
+    CartbarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/seachbar/seachbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seachbar',
+  templateUrl: './seachbar.component.html',
+  styleUrls: ['./seachbar.component.sass']
+})
+export class SeachbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
