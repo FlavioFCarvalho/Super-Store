32f01c6f0479ec2ce4b4185b5894375816f2c0c6
Flávio Carvalho <flaviocfr@gmail.com>
2018-06-30 17:23:00 -0300

    Create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { MenuComponent } from './components/menu/menu.component';
 import { LogoComponent } from './components/logo/logo.component';
 import { SeachbarComponent } from './components/seachbar/seachbar.component';
 import { CartbarComponent } from './components/cartbar/cartbar.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductCartComponent } from './components/product-cart/product-cart.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +19,11 @@ import { CartbarComponent } from './components/cartbar/cartbar.component';
     MenuComponent,
     LogoComponent,
     SeachbarComponent,
-    CartbarComponent
+    CartbarComponent,
+    BannerComponent,
+    ProductListComponent,
+    ProductCartComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
